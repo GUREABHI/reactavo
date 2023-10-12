21783aaf82c3c13c681f1c3de852ea38a2dc9778
Renzo Abarca <gustavo@gmail.com>
2023-10-12 09:29:56 -0500

    v3 mi nombre

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Gustavo Abarca
         </a>
       </header>
     </div>
