e4213909fa1c19df215970e7acdca0ac9f723418
Renzo Abarca <gustavo@gmail.com>
2023-10-19 09:49:54 -0500

    v3. instalo  y conf App,  AppForm, bootswatch, mat. icons, toastify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import C01Componente from './componentes/c01componente';
+import C01Componente from './componentes/C01componente';
+import C02variable from './componentes/C02variable';
+import Appform from './componentes/Appform';
 
 function App() {
   return (
-    <div>
-      <C01Componente/>
+    <div style={{background:"yellow", width:"350px", padding:"10px"}}>
+      <Appform/>
+      <i class="large material-icons">insert_chart</i>
     </div>
   );
 }
