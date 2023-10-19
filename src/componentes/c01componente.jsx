06b37491d8c1a98f93e735932c270cbd09daa4fb
Gustavo Abarca <gureabhi@gmail.com>
2023-10-18 22:42:08 -0500

    v.2 componente

---
+++ src/componentes/c01componente.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const c01componente = () => {
+  return (
+    <div>
+      <h1>Componente 01</h1>
+      pagina de prueba
+    </div>
+  )
+}
+
+export default c01componente
