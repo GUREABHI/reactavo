528c4ef9c7751325abcc2379cb32cd92f5ae76d8
Renzo Abarca <gustavo@gmail.com>
2023-10-27 09:16:08 -0500

    V7. Ya registra previa validacion y usa el idActual para Guardar o Actualizar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 //import logo from './logo.svg';
 //import './App.css';
-import {useState} from 'react';
+import {useEffect,useState} from 'react';
 import Appform from './componentes/Appform';
+import {db} from './firebase/firebase';
+import { query,collection, getDocs, doc ,onSnapshot,deleteDoc,where } from 'firebase/firestore';
 
 function App() {
 ///READ - LECTURA - fnread ///
-  const [docBD,setDocBD] = useState([]);
-  const fnread = () => {
+  const [docsBD,setDocsBD] = useState([]);
+
+  const fnRead = () => {
+    try{
+      const xColeccionConQuery =  query(collection(db, "persona"));
+      const unsubscribe = onSnapshot(xColeccionConQuery, (xDatosBD) => {
+        const xDoc = [];
+        xDatosBD.forEach((doc) => {
+          
+          xDoc.push({id: doc.id, ...doc.data()});
+        });
+        setDocsBD(xDoc);
+      });  
+    } catch (error) {
+      console.error(error);
+    }
     
   }
+//fnRead();
+//  console.log(docsBD);
+
 
 /// DELETE - eliminar - fnDelete ///
   const [idActual,setIdActual] = useState("");
   const fnDelete = (xId) => {
 
   }
+
 
    /// const camposRegistros = {nombre:"", edad:"", genero:""}///
   return (
